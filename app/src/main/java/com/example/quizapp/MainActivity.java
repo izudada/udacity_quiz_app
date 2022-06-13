8231eab1b512149567e16569f642d7d6529d99d5
Anthony <tonyudeagbala@gmail.com>
2022-06-13 15:02:52 +0100

    Set values for mthods

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -13,14 +13,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static String question [] = {
-
+        "How many computer languages are in use?",
+        "Who founded Apple Computer?",
+        "What does the Internet prefix WWW stand for?",
+        "A network designed to allow communication within an organization is called:",
+        "Which of these is not an early computer?"
     };
 
     public static String choices [][] = {
-
+            {"4312", "2000", "4000", "3200"},
+            {"Bill Gates", "Stephen Fry", "Steve Jobs", "Stephen Hawking"},
+            {"World Wide Web", "Western Washington World", "Worldwide Weather", "Wide Width Wickets"},
+            {"the Internet", "the World Wide Web", "Yahoo", "an intranet"},
+            {"NASA", "SAGE", "UNIVAC", "ENIAC"}
     };
 
     public static String correctAnswers [] = {
-
+        "2000",
+        "Steve Jobs",
+        "World Wide Web",
+        "an intranet",
+        "NASA"
     };
 }
